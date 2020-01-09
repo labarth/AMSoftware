5fdef9bb2e53bd53f2cdda5f63396ff01a8b21ce
alexander matiugin <alexander.matiugin@pandadoc.com>
2020-01-09 21:32:06 +0300

    deploy

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/AMSoftware'));
 
-app.get('/*', function(req,res) {
+app.get('/', function(req,res) {
 
   res.sendFile(path.join(__dirname,'/dist/AMSoftware/index.html'));
 });
