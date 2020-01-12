50f55426dfdb009d8b7a5c816298ffce8538d931
alexander matiugin <alexander.matiugin@pandadoc.com>
2020-01-12 20:26:31 +0300

    add mongodb

--- server/index.js
+++ server/index.js
@@ -6,12 +6,33 @@ import api from './api.js';
 
 
 const app = express();
-const dbUrl =  `${process.env.MONGODB_URI}/heroku_zk9xt1nk` || 'mongodb://localhost:27017/usersdb';
+const dbUrl =  process.env.MONGODB_URI ? `${process.env.MONGODB_URI}/heroku_zk9xt1nk` : 'mongodb://localhost:27017/my_database';
 mongoose.connect(dbUrl, { useNewUrlParser: true }, (error) => {
    if (error) throw error;
-
   console.log('successfully connection');
 });
+
+const db = mongoose.connection;
+console.log(db);
+const notesSchema = new mongoose.Schema({
+  id: String,
+  date: Number,
+  price: Number,
+  isNeedByu: Boolean,
+  description: String,
+});
+
+const Notes = mongoose.model('notes', notesSchema);
+
+Notes.find((err, zalupa) => {
+  console.log(zalupa, '@@@@@@@@@@@@@@@');
+
+  app.get('/zalupa/konya', function(req,res) {
+    res.send(zalupa);
+  });
+});
+
+
 
 app
   .use(cors())
