73f06bd542ed07f946e7d223b73832f51ef80817
alexander matiugin <alexander.matiugin@pandadoc.com>
2020-01-25 11:33:25 +0300

    add shared route

--- server/index.mjs
+++ server/index.mjs
@@ -1,6 +1,7 @@
 import express from 'express';
 import process from 'process';
 import bodyParser from 'body-parser';
+import path from 'path';
 import cors from 'cors';
 import './connect.mjs';
 import api from './api.mjs';
@@ -13,5 +14,8 @@ app
   .use(express.static('./dist/AMSoftware'))
   .use('/api', api);
 
+app.get('/*' , (req, res) => {
+  res.sendFile(path.join(path.resolve(), 'dist/AMSoftware/index.html'));
+})
 
 app.listen(process.env.PORT || 8080);
